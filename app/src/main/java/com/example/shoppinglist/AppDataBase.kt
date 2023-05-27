77fb5363a19d68f554d976e58dc25c6055fdbc96
MarcosLiberato <liberato-marcos@hotmail.com>
2023-05-27 10:23:48 -0300

    Init to commit

---
+++ app/src/main/java/com/example/shoppinglist/AppDataBase.kt
@@ -0,0 +1,9 @@
+package com.example.shoppinglist
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class AppDataBase: RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+}
---
+++ app/src/main/java/com/example/shoppinglist/TaskDao.kt
@@ -0,0 +1,32 @@
+package com.example.shoppinglist
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    //create
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(task: Task)
+
+    @Query("Select * from task")
+    fun getAll(): List<Task>
+
+    //update
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(task: Task)
+
+    //deleta todos
+    @Query("DELETE from task")
+    fun deleteAll()
+
+    //delete from id
+    @Query("DELETE from task WHERE id =:id")
+        fun deleteById(id: Int)
+
+}
